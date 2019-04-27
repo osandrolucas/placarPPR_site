dcc0e2a8199d88e683bf3a7059ab9d2bff480a90
Sandro Lucas <osandrolucas@gmail.com>
2019-04-27 18:47:49 -0300

    environment django Ok, and Post model

---
+++ placarPPR/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    publicador = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    meta_ano = models.TextField()
+    meta_acum = models.TextField()
+    real_acum = models.TextField()
+    percent_ating = models.TextField()
+    atingido_ano = models.TextField()
+    farol = models.TextField()
+    qt_salarios = models.TextField()
+    obs = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    published_date = models.DateTimeField(
+        blank=True, null=True)
+
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+
+def __str__(self):
+    return self.title
